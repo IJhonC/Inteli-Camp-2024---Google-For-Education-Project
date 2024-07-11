e8681494f5f16dabc0dfd98ab2e06169312af9a2
João Vitor Cristofolini <165334666+IJhonC@users.noreply.github.com>
2024-07-11 13:54:28 -0300

    final commit

---
+++ Google Educaction - LandingPage/JS/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("navbar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+document.getElementById('btn-faq').addEventListener('click', function () {
+  $('.accordion .collapse').collapse('show');
+});
+
+document.getElementById('btn-faq').addEventListener('click', function () {
+  $('.accordion .collapse').collapse('hide');
+});
